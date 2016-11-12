332a0a903edba781e176e2eba1e416cbb4b0935b
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2016-11-11 23:33:03 -0200

    Corrigindo sintax e movendo script para outro arquivo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,20 @@
-(function ($, window, document, undefined) {
+'use strict';
 
-  'use strict';
+function calculo() {
+	var distancia = document.getElementById('distancia').value;
+	var kmLitroCarro = document.getElementById('kmLitroCarro').value;
+	var precoGasolina = document.getElementById('precoGasolina').value;
 
-  $(function () {
-    // FastShell
-  });
+	if (!(kmLitroCarro.length === 0) && !(distancia.length === 0) && !(precoGasolina.length === 0)) {
+		var mediaConsumo = distancia / kmLitroCarro;
+		var mediaConsumoFinal = mediaConsumo.toFixed(2);
 
-})(jQuery, window, document);
+		var precoFinal = precoGasolina * mediaConsumoFinal;
+
+		document.getElementById('resultadoMediaConsumo').innerHTML = mediaConsumoFinal;
+		document.getElementById('resultadoPreco').innerHTML = precoFinal.toFixed(2);
+	}
+}
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', calculo);
