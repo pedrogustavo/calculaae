fc3ff66b39efd3087d4a271413eab90cd2655195
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2017-05-12 23:37:55 -0300

    Apply bulma styles

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React from 'react';
 const App = () => (
-  <div className="App">
-    <section className="col s12 m6 offset-m3">
-			<div className="row">
-		        <div className="col s12">
-		        	<label for="kmLitroCarro">Quantos Quilometros o carro anda por litro</label>
-		           	<input id="kmLitroCarro" type="text" />
-		        </div>
-	      	</div>
 
-			<div className="row">
-				<div className="col s12">
-					<label for="precoGasolina">Preço litro Gasolina</label>
-					<input type="text" id="precoGasolina" />
-				</div>
-			</div>
+  <section className="section">
+    <div className="columns is-desktop">
+      <div className="column is-half is-offset-one-quarter">
+        <h1 className="title">Calcula aê</h1>
+        <h2 className="subtitle">Saiba quanto você vai gastar com combustível</h2>
 
-			<div className="row">
-				<div className="col s12">
-					<label for="distancia">Distancia a ser percorrida (em KM)</label>
-					<input type="text" id="distancia" />
-				</div>
-			</div>
+        <div className="field">
+          <label className="label" htmlFor="kmLitroCarro">Quantos Quilometros seu carro anda por litro?</label>
+          <p className="control">
+            <input className="input" type="number" id="kmLitroCarro" placeholder="Digite o preço da gasolina" />
+          </p>
+        </div>
 
-			<button className="btn waves-effect waves-light" >Enviar</button>
-			<br />
-			<p className="flow-text">Média de consumo combustível:
-				<strong>
-					<span id="resultadoMediaConsumo" />
-					<small>litros</small>
-				</strong>
-			</p>
-			<p className="flow-text">Média do valor a ser gasto: <strong>R$<span id="resultadoPreco"></span></strong></p>
-		</section>
-  </div>
+        <div className="field">
+          <label className="label" htmlFor="precoGasolina">Preço da Gasolina (litro)</label>
+          <p className="control">
+            <input className="input" type="text" id="precoGasolina" placeholder="Digite o preço da gasolina" />
+          </p>
+        </div>
+
+        <div className="field">
+          <label className="label" htmlFor="distancia">Distancia a ser percorrida (em KM)</label>
+          <p className="control">
+            <input className="input" type="text" id="distancia" placeholder="Digite a distância percorrida" />
+          </p>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <button className="button is-primary">Enviar</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default App;
