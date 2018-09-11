67fd1347bfd83f9bfa0dba8766b660dbbab0e1e4
Pedro Gustavo <eu@pedrogustavo.com>
2018-09-11 06:16:00 +0000

    :lipstick: Adiciona tema default do vue material

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 
